4d5138eeabf5ff95be98f89de83a2959a4d84d57
Jonathan <theehakan@gmail.com>
2024-08-23 17:01:04 -0400

    Mnor bug fixes

--- serverFiles/utils/jwtGenerator.js
+++ serverFiles/utils/jwtGenerator.js
@@ -16,19 +16,3 @@ function jwtGenerator(user_id) {
 
 module.exports = jwtGenerator;
 
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-};
-
-const jwt = require('jsonwebtoken');
-
-
-function jwtGenerator(user_id) {
-    const payload = {
-        user: user_id
-    };
-
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' })
-}
-
-module.exports = jwtGenerator;
