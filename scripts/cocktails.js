671dfaae97a484669efc766878081854c7d34054
Jonathan <theehakan@gmail.com>
2024-09-01 16:58:19 -0400

    minor fix

--- scripts/cocktails.js
+++ scripts/cocktails.js
@@ -169,6 +169,15 @@ function checkCocktialInputs(name, spirit, recipe){
         return;
     };
 
+    //check if duplicate cocktail
+    for (let cocktail of cocktails) {
+        if (cocktailNameInput.value.toLowerCase() === cocktail.cocktail_name.toLowerCase()) {
+            cocktailErrorModal.showModal();
+            cocktailErrorText.innerText = 'Cocktail already exists on list';
+            return;
+        };
+    };
+
     //sends data to functions based of button value
     if (submitCocktail.value === 'Add Cocktail') {
         addCocktail(name, spirit, ingredients, recipe);
